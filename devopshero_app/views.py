f2d44aa6b2c570749870ee9e327e6bae353d5de3
Victor Mendiluce <vmendi@gmail.com>
2025-12-20 15:45:15 -0800

    Browser Reload

--- devopshero_app/views.py
+++ devopshero_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the devopshero index.")
+    return render(request, "devopshero_app/index.html")
 
