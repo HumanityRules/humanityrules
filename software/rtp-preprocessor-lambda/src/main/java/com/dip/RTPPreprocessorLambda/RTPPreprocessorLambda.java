c7be8dc76a146bfb676b5da6a4c4e308dcd38768
Victor Mendiluce <vmendi@gmail.com>
2026-01-14 09:26:05 -0800

    Squashed 'deployable_repos/public/file_processing_service/' content from commit eb3d081a6
    git-subtree-dir: deployable_repos/public/file_processing_service
    git-subtree-split: eb3d081a67698ce9b163e9bac9e5f45138554881

---
+++ software/rtp-preprocessor-lambda/src/main/java/com/dip/RTPPreprocessorLambda/RTPPreprocessorLambda.java
@@ -0,0 +1,14 @@
+package com.dip.RTPPreprocessorLambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class RTPPreprocessorLambda implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+        return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("Hello Api");
+    }
+}
